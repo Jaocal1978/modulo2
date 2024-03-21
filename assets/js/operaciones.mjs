8adfaaa95972ea0688343fcdc02c82daebb1223b
Jorge Olave <jorge.olave.c@gmail.com>
2024-03-21 11:20:56 -0300

    Repositorio Modulo2

---
+++ assets/js/operaciones.mjs
@@ -0,0 +1,18 @@
+function potencia(base, exponente) 
+{
+	return Math.pow(base, exponente);
+}
+
+function sumar(x, y)
+{
+	return x + y;
+}
+
+function multiplicar(x, y)
+{
+	return x * y;
+}
+
+const MASA_TIERRA = 1000000;
+
+export {potencia, sumar, multiplicar, MASA_TIERRA}
---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+import {potencia, sumar, multiplicar, MASA_TIERRA} from "./operaciones.mjs";
+
+console.log(sumar);
+console.log(MASA_TIERRA);
+console.log(multiplicar);
+console.log(potencia);
